0331086e6050c783d010a0e5bc751a4576a7f2a3
AKras <Andrey0961414@gmail.com>
2022-07-24 10:51:37 +0300

    Add appsettings

--- CartingService.Api.Web/Service/KafkaConsumerHostedService.cs
+++ CartingService.Api.Web/Service/KafkaConsumerHostedService.cs
@@ -10,18 +10,21 @@ using BLL.Service;
 using AutoMapper;
 using CartingService.Api.BLL.Models;
 using CartingService.Api.Web.Models;
+using Microsoft.Extensions.Configuration;
 
 namespace CartingService.Api.Web.Service
 {
     public class KafkaConsumerHostedService : IHostedService
     {
         private readonly ILogger<KafkaConsumerHostedService> logger;
+        private IConfiguration configuration;
         private readonly IMessageService messageService;
         private readonly IMapper mapper;
         private ClusterClient cluster;
-        public KafkaConsumerHostedService(IMapper mapper, ILogger<KafkaConsumerHostedService> logger, IMessageService messageService)
+        public KafkaConsumerHostedService(IMapper mapper, ILogger<KafkaConsumerHostedService> logger, IMessageService messageService, IConfiguration configuration)
         {
             this.messageService = messageService;
+            this.configuration = configuration;
             this.mapper = mapper;
             this.logger = logger;
             cluster = new ClusterClient(new Configuration()
@@ -32,7 +35,8 @@ namespace CartingService.Api.Web.Service
 
         public Task StartAsync(CancellationToken cancellationToken)
         {
-            cluster.ConsumeFromLatest("demo1");
+            var host = configuration.GetSection("Kafka").GetValue<string>("Host");
+            cluster.ConsumeFromLatest(host);
             cluster.MessageReceived += record =>
             {
                 var productMessage = JsonConvert.DeserializeObject<ProductMessage>(Encoding.UTF8.GetString(record.Value as byte[]));
